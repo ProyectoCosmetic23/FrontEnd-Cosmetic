b8db73ce34196d2730643bd92681255e61eb9770
julian carreno <julianctsistemas@gmail.com>
2023-11-02 20:42:48 -0500

    pruebas

--- src/app/views/employees/employee-routing.module.ts
+++ src/app/views/employees/employee-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
         component: EmployeeListComponent
     },
     {
-        path: 'crearEmpleado',
+        path: 'new',
         component: EmployeeDetailComponent
     },
     {
