cbc596589a35d2b7dc14436cf606fde117c4b79b
marianagrasi <marianagranados201@gmail.com>
2023-11-08 07:32:44 -0500

    Clientes

---
+++ src/app/shared/services/client.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+@Injectable({
+    providedIn: 'root'
+})
+export class ClientsService {
+    url = 'http://localhost:8080/api/clients';
+
+    constructor(private http: HttpClient) { }
+
+    getAllClients(): Observable<any> {
+        return this.http.get(this.url);
+    }
+
+    createClient(client: any): Observable<any> {
+        return this.http.post(this.url, client).pipe(
+            catchError((error: HttpErrorResponse) => {
+                console.error('Error en la solicitud:', error);
+                return throwError('Ocurrió un error al crear el cliente. Por favor, inténtalo de nuevo.');
+            })
+        );
+    }
+
+    updateClient(id: number, updatedData: any): Observable<any> {
+        return this.http.put(`${this.url}/${id}`, updatedData).pipe(
+            catchError((error: HttpErrorResponse) => {
+                console.error('Error en la solicitud:', error);
+                return throwError('Ocurrió un error al actualizar el cliente. Por favor, inténtalo de nuevo.');
+            })
+        );
+    }
+
+
+
+
+    checkCedulaAvailability(cedula: string): Observable<boolean> {
+        return this.http.get<boolean>(`${this.url}-check-cedula?cedula=${cedula}`);
+    }
+
+    checkEmailAvailability(email: string): Observable<boolean> {
+        return this.http.get<boolean>(`${this.url}-check-email?email=${email}`);
+    }
+
+    getClientsById(id: number): Observable<any> {
+        return this.http.get<any>(`${this.url}/${id}`);
+    }
+
+
+    clientChangeStatus(id: any): Observable<any> {
+        return this.http.put<boolean>(`${this.url}/changeState/${id}`, {});
+
+    }
+
+} 
