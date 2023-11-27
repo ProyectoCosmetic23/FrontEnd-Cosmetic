40b5969a68ac5d79169973e3738cd5b112c381ac
MarcelaM18 <marcelamorales1808@gmail.com>
2023-11-27 05:56:46 -0500

    boton disable cuan esta inactivo user

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url:'https://api-cosmetic-1iuc.onrender.com',
+  url:'https://api-cosmetic-qqce.onrender.com',
 // url:'http://localhost:8080'
 };
 
