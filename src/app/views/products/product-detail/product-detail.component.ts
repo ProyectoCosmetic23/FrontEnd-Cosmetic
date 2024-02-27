5c0062a5a78c7074f1fede67601b4d2985bad1c8
julian carreno <julianctsistemas@gmail.com>
2024-02-26 20:22:40 -0500

    fix

--- src/app/views/products/product-detail/product-detail.component.ts
+++ src/app/views/products/product-detail/product-detail.component.ts
@@ -99,12 +99,12 @@ export class ProductDetailComponent implements OnInit {
         [Validators.required, Validators.maxLength(80)],
         [this.validateNameSimbolAndNumber],
       ],
-      quantity: [0], // Establece el valor inicial en 0
-      max_stock: [0, [Validators.required, this.validateNonNegative]], // Establece el valor inicial en 0 y agrega validador requerido y validador de no negativos
-      min_stock: [0, [Validators.required, this.validateNonNegative]], // Establece el valor inicial en 0 y agrega validador requerido y validador de no negativos
+      quantity: [null, [Validators.required]], // Establece el valor inicial en 0
+      max_stock: [null, [Validators.required, this.validateNonNegative]], // Establece el valor inicial en 0 y agrega validador requerido y validador de no negativos
+      min_stock: [null, [Validators.required, this.validateNonNegative]], // Establece el valor inicial en 0 y agrega validador requerido y validador de no negativos
       profit: [],
-      cost_price: [0, [Validators.required, Validators.min(0)]], // Establece el valor inicial en 0 y agrega validador mínimo
-      selling_price: [0, [Validators.required, Validators.min(0)]], // Establece el valor inicial en 0 y agrega validador mínimo
+      cost_price: [null, [Validators.required, Validators.min(0)]], // Establece el valor inicial en 0 y agrega validador mínimo
+      selling_price: [null, [Validators.required, Validators.min(0)]], // Establece el valor inicial en 0 y agrega validador mínimo
       observation: ["", [Validators.maxLength(100)]],
       state_product: [],
       creation_date_product: [],
@@ -374,16 +374,24 @@ export class ProductDetailComponent implements OnInit {
       this.loading = true;
       setTimeout(() => {
         this.loading = false;
-        this.toastr.success("Producto modificado con éxito.", "Éxito", {
-          progressBar: true,
-          timeOut: 3000,
-        });
+        if (this.viewMode === 'new') {
+          this.toastr.success("Producto registrado con éxito.", "Éxito", {
+            progressBar: true,
+            timeOut: 3000,
+          });
+        } else if (this.viewMode === 'edit') {
+          this.toastr.success("Producto modificado con éxito.", "Éxito", {
+            progressBar: true,
+            timeOut: 3000,
+          });
+        }
         setTimeout(() => {
           this.router.navigateByUrl("/products");
         });
       });
     }
   }
+  
 
 
 
